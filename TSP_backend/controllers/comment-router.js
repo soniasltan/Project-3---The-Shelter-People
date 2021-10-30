a67d4ee0052d5fcc8db8cd7f8fb8d007b0e9cdbd
Stabilo <zteo015@e.ntu.edu.sg>
2021-10-30 17:25:23 +0800

    updated comments for cat and comments router

--- TSP_backend/controllers/comment-router.js
+++ TSP_backend/controllers/comment-router.js
@@ -10,18 +10,21 @@ const CommentCtrl = require("./comment-ctrl");
 //              POST ROUTES
 // =======================================
 // This is for new comment for a particular cat
+// :id is the cat's id
 router.post("/cats/:id/newcomment", CommentCtrl.createComment);
 
 // =======================================
 //              PUT ROUTES
 // =======================================
 // This is for updating a comment
+// :id is the comment's id
 router.put("/comments/:id", CommentCtrl.updateComment);
 
 // =======================================
 //              DELETE ROUTES
 // =======================================
 // delete comment
+// :id is the comment's id
 router.delete("/comments/:id", CommentCtrl.deleteComment);
 
 module.exports = router;
