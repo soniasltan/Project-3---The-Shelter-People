ac8373502f6e3758fbc18d6c9576a63ece117d98
Stabilo <zteo015@e.ntu.edu.sg>
2021-10-30 23:34:03 +0800

    edited comment controller, now when create/edit comment, cat model will be updated too

--- TSP_backend/controllers/comment-ctrl.js
+++ TSP_backend/controllers/comment-ctrl.js
@@ -9,7 +9,7 @@ const Cat = require("../models/cats");
 
 // For making a new comment
 // When making new comment, have to attach comment to particular cat
-const createComment = (req, res) => {
+const createComment = async (req, res) => {
   // if there is no req.body, return error
   if (!req.body) {
     return res.status(400).json({
@@ -17,30 +17,34 @@ const createComment = (req, res) => {
       error: "You must provide a comment",
     });
   }
-  // req.body exists, so make a new comment
-  const comment = new Comment(req.body);
-
-  // somehow, if the new comment doesn't exist, return error
-  if (!comment) {
-    return res.status(400).json({ success: false, error: err });
-  }
-  // comment exists! So insert it in database
-  comment
-    .save()
-    .then(() => {
-      // return json response if successful
-      return res.status(201).json({
-        success: true,
-        id: comment._id,
-        message: "Comment created!",
+  await Cat.findById(req.params.id, (err, foundCat) => {
+    // req.body exists, so make a new comment
+    const comment = new Comment(req.body);
+    // Append the comment to the cat
+    foundCat.comments.push(comment);
+    foundCat.save((err, data) => console.log(data));
+    // somehow, if the new comment doesn't exist, return error
+    if (!comment) {
+      return res.status(400).json({ success: false, error: err });
+    }
+    // comment exists! So insert it in database
+    comment
+      .save()
+      .then(() => {
+        // return json response if successful
+        return res.status(201).json({
+          success: true,
+          id: comment._id,
+          message: "Comment created!",
+        });
+      })
+      .catch((error) => {
+        return res.status(400).json({
+          error,
+          message: "Comment not created!",
+        });
       });
-    })
-    .catch((error) => {
-      return res.status(400).json({
-        error,
-        message: "Comment not created!",
-      });
-    });
+  });
 };
 
 // For updating comment
@@ -53,35 +57,40 @@ const updateComment = async (req, res) => {
     });
   }
 
-  // req.body exists, so find the comment by id and then update
-  Comment.findOne({ _id: req.params.id }, (err, comment) => {
-    if (err) {
+  try {
+    // req.body exists, so find the comment by id and then update
+    const comment = await Comment.findById(req.params.id);
+    if (!comment) {
       return res.status(404).json({
         err,
         message: "Comment not found!",
       });
     }
-    console.log(req.body);
     // update the comment details
     comment.text = req.body.text;
     // save the updated comment
-    comment
-      .save()
-      .then(() => {
-        // return json response if successful
-        return res.status(200).json({
-          success: true,
-          id: comment._id,
-          message: "Comment updated!",
-        });
-      })
-      .catch((error) => {
-        return res.status(404).json({
-          error,
-          message: "Comment not updated!",
-        });
-      });
-  });
+    await comment.save();
+
+    // now update the comment entry for the cat too
+    const cat = await Cat.findById(comment.cat_id);
+    // remove the old, non-updated comment first
+    cat.comments.id(comment._id).remove();
+    // now add in the updated comment
+    cat.comments.push(comment);
+
+    await cat.save();
+
+    res.status(200).json({
+      success: true,
+      id: comment._id,
+      message: "Comment updated!",
+    });
+  } catch (err) {
+    res.status(404).json({
+      error,
+      message: "Comment not updated!",
+    });
+  }
 };
 
 // For deleting comment
