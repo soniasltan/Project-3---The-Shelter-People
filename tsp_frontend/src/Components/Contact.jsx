8355c59c6823c5da3945c07e0fb2edb2cddd8b14
cheryltjh <xfreakyn@gmail.com>
2021-11-01 09:49:16 +0800

    updated contact

--- tsp_frontend/src/Components/Contact.jsx
+++ tsp_frontend/src/Components/Contact.jsx
@@ -10,7 +10,7 @@ function Contact() {
   return (
     <>
       <h1>Contact us!</h1>
-      <P>We are located at Animal Lodge @ 59 Sungei Tengah Road, Block V, Singapore 699014)</P>
+      <P>We are located at Animal Lodge @ 59 Sungei Tengah Road, Block V, Singapore 699014</P>
       <P>Would you love to volunteer? Contact us today at ______</P>
     </>
   );
