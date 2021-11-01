43baf8661a1e58e765b9b17c886b1ddec1317887
cheryltjh <xfreakyn@gmail.com>
2021-11-01 11:19:49 +0800

    Home text updated

--- tsp_frontend/src/Components/Home.jsx
+++ tsp_frontend/src/Components/Home.jsx
@@ -10,7 +10,8 @@ function Home() {
     <>
       <h1>Hello!</h1>
       <h2>Welcome to TheShelterPeople's website!</h2>
-      <P>Do have fun getting to know the cats here!</P>
+      <P>We have many cute cats here!</P>
+      <P>Do have fun getting to know the cats</P>
     </>
   );
 }
