19a25fa0c4c04ab76ad809a7e30f5a4d5719a586
Stabilo <zteo015@e.ntu.edu.sg>
2021-11-02 15:18:44 +0800

    edited contact.jsx so that link opens in new tab

--- tsp_frontend/src/Components/Contact.jsx
+++ tsp_frontend/src/Components/Contact.jsx
@@ -11,7 +11,7 @@ function Contact() {
     <>
       <h1>Contact us!</h1>
       <P>We are located at The Animal Lodge @ 59 Sungei Tengah Road, Singapore 699014</P>
-      <P>Would you love to volunteer? Check out our instagram <a href = "https://www.instagram.com/theshelterpeople_sg/">@theshelterpeople_sg</a> </P>
+      <P>Would you love to volunteer? Check out our instagram <a href = "https://www.instagram.com/theshelterpeople_sg/" target="_blank">@theshelterpeople_sg</a> </P>
     </>
   );
 }
