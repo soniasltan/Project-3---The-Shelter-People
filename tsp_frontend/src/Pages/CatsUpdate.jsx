0de79355cfbc5fdd1b2a5c0f2b927d86f60c6f79
cheryltjh <xfreakyn@gmail.com>
2021-11-03 01:42:56 +0800

    form updated

--- tsp_frontend/src/Pages/CatsUpdate.jsx
+++ tsp_frontend/src/Pages/CatsUpdate.jsx
@@ -1,81 +1,112 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 
 //Change to Controlled Form to repopulate the value
 
 function CatsUpdate() {
+  const history = useHistory();
   const { id } = useParams();
-  const [catDetail, setCatDetail] = useState("");
-  const [inputCatName, setInputCatName] = useState("");
-  const [inputCatDescription, setInputCatDescription] = useState("");
-  const [inputCatImage, setInputCatImage] = useState("");
-  const [inputCatGender, setInputCatGender] = useState("");
-  const [inputCatAdopt, setInputCatAdopt] = useState("");
-  const [inputCatCage, setInputCatCage] = useState("");
 
-  // get the cat data for the update form
+  const [catDetail, setCatDetail] = useState({});
+  // const [inputCatName, setInputCatName] = useState("");
+  // const [inputCatDescription, setInputCatDescription] = useState("");
+  // const [inputCatImage, setInputCatImage] = useState("");
+  // const [inputCatGender, setInputCatGender] = useState("");
+  // const [inputCatAdopt, setInputCatAdopt] = useState("");
+  // const [inputCatCage, setInputCatCage] = useState("");
+
+  // get the cat data for the update forms
   useEffect(() => {
-    async function getCatData() {
-      await axios.get(`http://localhost:3000/api/cats/${id.id}`).then((cat) => {
-        setCatDetail(cat.data.data);
-      });
-    }
-    getCatData();
-  }, []);
+    const getCatData = async (id) => {
+      const url = `http://localhost:3000/api/cats/${id}`;
+      const catData = await axios.get(url);
+      setCatDetail(catData.data);
+    };
+    getCatData(id);
+  }, [id]);
 
-  const handleUpdate = async () => {
-    await axios
-      .put(`http://localhost:3000/api/cats/${id._id}`, {
-        inputCatName,
-        inputCatDescription,
-        inputCatImage,
-        inputCatGender,
-        inputCatAdopt,
-        inputCatCage,
-      })
+  // useEffect(() => {
+  //   async function getCatData() {
+  //     await axios.get(`http://localhost:3000/api/cats/${id}`).then((cat) => {
+  //       setCatDetail(cat.data.data);
+  //     });
+  //   }
+  //   getCatData();
+  // }, []);
+
+  const changeName = (event) => {
+    const value = event.target.value;
+    setCatDetail({ ...catDetail, Name: value });
+  };
+
+  const changeDescription = (event) => {
+    const value = event.target.value;
+    setCatDetail({ ...catDetail, Description: value });
+  };
+
+  const handleUpdate = async (event) => {
+    event.preventDefault();
+    // const name = inputCatName;
+    // const description = inputCatDescription;
+    // const image = inputCatImage;
+    // const gender = inputCatGender;
+    // const adoptable = inputCatAdopt;
+    // const cage = inputCatCage;
+
+    // const catDetail = { name, description, image, gender, adoptable, cage };
+      await axios.put(`http://localhost:3000/api/cats/${catDetail._id}`, catDetail)
       .then((res) => {
         window.alert(`Cat updated successfully!`);
+        history.push(`/cats/list`);
       });
   };
+
+// await axios.put(`http://localhost:3000/api/cats/${catDetail._id}`, catDetail)
+// .then((res) => {
+//   window.alert(`Cat updated successfully!`);
+//   history.push(`/cats/list`);
+// });
+// };
+// }
 
   return (
     <div>
       <p>In this page you'll see the form to update a cat</p>
+      <form onSubmit={handleUpdate}>
       <label>Name:</label>
       <input
         type="text"
         placeholder="name"
-        value={inputCatName.name}
-        onChange={(e) => setInputCatName(e.target.value)}
+        value={catDetail.name}
+        onChange={changeName}
       />
       <label>Description:</label>
       <input
-        type="text"
+        type="textarea"
         placeholder="description"
-        value={inputCatDescription.description}
-        onChange={(e) => setInputCatDescription(e.target.value)}
+        value={catDetail.description}
+        onChange={changeDescription}
       />
       <label>Image:</label>
       <input
         type="text"
         placeholder="imageURL"
-        value={inputCatImage.image}
-        onChange={(e) => setInputCatImage(e.target.value)}
+        value={catDetail.image}
       />
       <label>Gender:</label>
-      <select value={inputCatGender.gender} >
+      <select value={catDetail.gender}>
         <option value="Male">Male</option>
         <option value="Female">Female</option>
         <option value="Unknown">Unknown</option>
       </select>
       <label>Adopt:</label>
-      <select value={inputCatAdopt.adoptable}>
+      <select value={catDetail.adoptable}>
         <option value="Yes">Yes</option>
         <option value="No">No</option>
       </select>
       <label>Cage:</label>
-      <select value={inputCatCage.cage}>
+      <select value={catDetail.cage}>
         <option value="6/7">6/7</option>
         <option value="2">2</option>
         <option value="3">3</option>
@@ -84,7 +115,9 @@ function CatsUpdate() {
         <option value="8">8</option>
         <option value="9">9</option>
       </select>
-      <input type="submit" onClick={handleUpdate} value="update cat" />
+      <button>Update Cat</button>
+      <button href={'/cats/list'}>Cancel</button>
+      </form>
     </div>
   );
 }
