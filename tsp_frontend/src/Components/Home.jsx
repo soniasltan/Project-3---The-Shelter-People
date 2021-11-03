d1aedf9f324b3daa03a18d0677ccbe09cd247d5d
cheryltjh <xfreakyn@gmail.com>
2021-11-03 17:35:11 +0800

    Home page edited

--- tsp_frontend/src/Components/Home.jsx
+++ tsp_frontend/src/Components/Home.jsx
@@ -1,14 +1,26 @@
 import React from "react";
+import catimg from "../Components/catimg.jpg";
 import styled from "styled-components";
 
-// const P = styled.p`
-//   margin: 8px;
+// const Container = styled.div`
+//   background-position: center;
+//   background-repeat: no-repeat;
+//   background-size: cover;
+//   @media only screen and (max-width: 1600px) {
+//     height: 90vh;
+//   }
+// `;
+
+// const Content = styled.div`
+//     background-image: url(${catimg});
+//     width: auto;
+//     height: auto;
 // `;
 
 function Home() {
   return (
     <>
-      <h1>Hello!</h1>
+    <h1>Hello!</h1>
     </>
   );
 }
