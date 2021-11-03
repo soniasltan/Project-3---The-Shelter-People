441a8d69bc41aec4bbe897ffc416b975c9b08498
“soniatan” <sonnyeah@gmail.com>
2021-11-03 15:32:21 +0800

    uncommented google map

--- tsp_frontend/src/Components/Contact.jsx
+++ tsp_frontend/src/Components/Contact.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-// import { GoogleMap, Marker, LoadScript } from "@react-google-maps/api";
+import { GoogleMap, Marker, LoadScript } from "@react-google-maps/api";
 // import InstagramEmbed from 'react-instagram-embed'
 
 const P = styled.p`
@@ -28,11 +28,11 @@ function Contact() {
     <>
       <h1>Contact us!</h1>
       <P>We are located at The Animal Lodge @ 59 Sungei Tengah Road, Singapore 699014</P>
-      {/* <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLEMAPS_API_KEY}>
+      <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLEMAPS_API_KEY}>
       <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={12}>
         <Marker position={position} />
       </GoogleMap>
-    </LoadScript> */}
+    </LoadScript>
       <P>Interested in volunteering? Check out our Instagram <a href = "https://www.instagram.com/theshelterpeople_sg/" target="_blank" rel="noreferrer">@theshelterpeople_sg</a> </P>
       {/* <InstagramEmbed
   url='https://instagr.am/p/Zw9o4/'
