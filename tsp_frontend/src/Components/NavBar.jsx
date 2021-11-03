c2d00742a61716c195bc178cbc30cd01c5579e2a
cheryltjh <xfreakyn@gmail.com>
2021-11-04 02:33:14 +0800

    NavBar Login button css updated

--- tsp_frontend/src/Components/NavBar.jsx
+++ tsp_frontend/src/Components/NavBar.jsx
@@ -4,11 +4,12 @@ import styled from "styled-components";
 
 const Navbar = styled.nav`
   display: flex;
+  position: sticky;
   align-items: center;
   justify-content: center;
   width: 100%;
   height: 6vh;
-  background-color: beige;
+  background-color: #faf0e6;
   box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.15);
 `;
 
@@ -20,18 +21,66 @@ const Li = styled.li`
   font-size: 16px;
   border-radius: 24px;
   &:hover {
-    background-color: #EFBE93;
+    background-color: #efbe93;
   }
   &:active {
-    background-color: #EFBE93;
+    background-color: #efbe93;
   }
 `;
 
 const LinkStyled = styled(Link)`
   text-decoration: none;
   color: black;
+  font-weight: bold;
+  font-size: 2rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 100%;
+  transition: all 0.2s ease;
 `;
 
+// const NavBtn = styled(Link)`
+//   text-decoration: none;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   padding: 8px 16px;
+//   height: 100%;
+//   width: 100%;
+//   border: none;
+//   outline: none;
+// `;
+
+const Button = styled.button`
+  border-radius: ${({ bigRadius }) => (bigRadius ? "30px" : "20px")};
+  background-color: ${({ primary }) => (primary ? "#EFBE93" : "#000")};
+  color: ${({ primary }) => (primary ? "#000" : "#fff")};
+  padding: ${({ big }) => (big ? "15px 27px" : "5px 23px")};
+  font-size: ${({ bigFont }) => (bigFont ? "20px" : "18px")};
+  outline: none;
+  cursor: pointer;
+  border: none;
+  transition: all 0.5s ease;
+
+  &:hover {
+    background-color: ${({ primary }) => (primary ? "#fff" : "#E38B06")};
+    transform: translateY(-0.5rem) scale(1.02);
+    color: #000;
+  }
+  &:active {
+    transform: translateY(0.5rem);
+  }
+
+  @media only screen and (max-width: 1000px) {
+    /* width: 100%; */
+    padding: ${({ big }) => (big ? "18px 30px" : "10px 20px")};
+  }
+  @media only screen and (max-width: 375px) {
+    padding: ${({ big }) => (big ? "12px 20px" : "10px 20px")};
+    font-size: ${({ bigFont }) => (bigFont ? "16px" : "18px")};
+  }
+`;
 
 function NavBar({ role, auth, handleLogOut, userName }) {
   return (
@@ -44,10 +93,10 @@ function NavBar({ role, auth, handleLogOut, userName }) {
           <Li>Cats</Li>
         </LinkStyled>
         <LinkStyled to="/about" className="about">
-          <Li>About us</Li>
+          <Li>About Us</Li>
         </LinkStyled>
         <LinkStyled to="/contact" className="contact">
-          <Li>Contact us</Li>
+          <Li>Contact Us</Li>
         </LinkStyled>
         {auth === "NoAuth" ? (
           <>
@@ -55,7 +104,7 @@ function NavBar({ role, auth, handleLogOut, userName }) {
               <Li>Signup</Li>
             </LinkStyled>
             <LinkStyled to="/login" className="login">
-              <Li>Login</Li>
+              <Button primary>Login</Button>
             </LinkStyled>
           </>
         ) : (
