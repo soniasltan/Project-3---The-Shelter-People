d3e12531e6c1edd149d819558cf98d021b71e4e4
cheryltjh <xfreakyn@gmail.com>
2021-11-04 23:04:26 +0800

    removed styling from commentupdate

--- tsp_frontend/src/Pages/CommentUpdate.jsx
+++ tsp_frontend/src/Pages/CommentUpdate.jsx
@@ -1,39 +1,8 @@
+import MDEditor from "@uiw/react-md-editor";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useHistory, useParams } from "react-router-dom";
-import styled from "styled-components";
-import MDEditor from "@uiw/react-md-editor";
-
-const Button = styled.button`
-  font-family: "Spartan", sans-serif;
-  font-weight: bold;
-  padding: 10px;
-  margin: 6px 2px;
-  border: none;
-  border-radius: 6px;
-  box-sizing: border-box;
-  cursor: pointer;
-  font-size: 16px;
-  background-color: #EFBE93;
-  @media only screen and (max-width: 600px) {
-    border: none;
-    border-radius: 6px;
-    box-sizing: border-box;
-    cursor: pointer;
-    font-size: 14px;
-    position: relative;
-  }
-  &:hover {
-    background-color: rgb(228, 228, 228);
-  }
-  &:active {
-    background-color: grey};
-  }
-`;
-
-const Container = styled.div`
-  width: 500px;
-`;
+import { Button, Container } from "../Styles/CommentsUpdateStyle";
 
 function CommentUpdate() {
   let id = useParams();
@@ -73,8 +42,8 @@ function CommentUpdate() {
     <>
       <div>
         <Container>
-        <h3>Edit Comment</h3>
-        <MDEditor value={value} onChange={setValue} />
+          <h3>Edit Comment</h3>
+          <MDEditor value={value} onChange={setValue} />
         </Container>
       </div>
       <Button onClick={() => updateComment()}>Update</Button>
