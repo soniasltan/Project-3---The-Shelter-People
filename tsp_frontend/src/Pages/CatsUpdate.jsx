08c44bbc735878faa764b278f6dcfbf949b9e084
“soniatan” <sonnyeah@gmail.com>
2021-11-04 18:21:08 +0800

    created new styles folder under frontend src and moved page styling to separate files for readability

--- tsp_frontend/src/Pages/CatsUpdate.jsx
+++ tsp_frontend/src/Pages/CatsUpdate.jsx
@@ -1,117 +1,19 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams, useHistory } from "react-router-dom";
-import styled from "styled-components";
-
-const Form = styled.form`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Label = styled.label`
-  margin: 7px 5px 8px 5px;
-`;
-
-const DescriptionLabel = styled.label`
-  margin: 40px 5px 43px 5px;
-`;
-
-const CatInfo = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin-bottom: 15px;
-`;
-
-const LabelContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: flex-start;
-  margin-top: 2.5px;
-`;
-
-const InputContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-
-const Input = styled.input`
-font-family: "Spartan", sans-serif;
-  margin: 5px;
-  padding: 2px;
-  border: 1px solid black;
-  border-radius: 6px;
-  box-sizing: border-box;
-  cursor: pointer;
-  font-size: 16px;
-  @media only screen and (max-width: 600px) {
-    border: 1px solid black;
-    border-radius: 6px;
-    box-sizing: border-box;
-    cursor: pointer;
-    font-size: 14px;
-    position:relative;
-  }
-`;
-
-const DescriptionInput = styled.textarea`
-font-family: "Spartan", sans-serif;
-  height: 90px;
-  resize: none;
-  margin: 5px;
-  padding: 2px;
-  border: 1px solid black;
-  border-radius: 6px;
-  box-sizing: border-box;
-  cursor: pointer;
-  font-size: 16px;
-  @media only screen and (max-width: 600px) {
-    border: 1px solid black;
-    border-radius: 6px;
-    box-sizing: border-box;
-    cursor: pointer;
-    font-size: 14px;
-    position:relative;
-  }
-`;
-
-const Select = styled.select`
-  margin: 5px;
-  border-radius: 6px;
-  padding: 2px;
-  cursor: pointer;
-  font-family: "Spartan", sans-serif;
-`;
-
-const Button = styled.button`
-  font-family: "Spartan", sans-serif;
-  font-weight: bold;
-  padding: 10px;
-  margin: 7px 2px;
-  border: none;
-  border-radius: 6px;
-  box-sizing: border-box;
-  cursor: pointer;
-  font-size: 16px;
-  background-color: #EFBE93;
-  @media only screen and (max-width: 600px) {
-    border: none;
-    border-radius: 6px;
-    box-sizing: border-box;
-    cursor: pointer;
-    font-size: 14px;
-    position: relative;
-  }
-  &:hover {
-    background-color: rgb(228, 228, 228);
-  }
-  &:active {
-    background-color: grey};
-  }
-`;
+import {
+  Form,
+  Label,
+  DescriptionLabel,
+  CatInfo,
+  LabelContainer,
+  InputContainer,
+  Input,
+  DescriptionInput,
+  Select,
+  CancelLink,
+  Button,
+} from "../Styles/CatsUpdateStyle";
 
 function CatsUpdate({ role, auth }) {
   const history = useHistory();
@@ -167,72 +69,79 @@ function CatsUpdate({ role, auth }) {
       <h1>Update Cat Details</h1>
       <Form onSubmit={handleUpdate}>
         <CatInfo>
-        <LabelContainer>
-        <Label>Name:</Label>
-        <DescriptionLabel>Description:</DescriptionLabel>
-        <Label>Image URL:</Label>
-        <Label>Gender:</Label>
-        <Label>Adoptable:</Label>
-        <Label>Cage:</Label>
-        </LabelContainer>
-        <InputContainer>
-        <Input
-          type="text"
-          name="name"
-          minLength="2"
-          value={updateCatDetail.name}
-          onChange={(event) => handleChange(event)}
-          required
-        />
-        <DescriptionInput
-          type="textarea"
-          name="description"
-          minLength="1"
-          value={updateCatDetail.description}
-          onChange={(event) => handleChange(event)}
-          required
-        />
-        <Input
-          type="text"
-          name="image"
-          minLength="5"
-          value={updateCatDetail.image}
-          onChange={(event) => handleChange(event)}
-        />
-        <Select name="gender" onChange={(event) => handleChange(event)}>
-          <option value={updateCatDetail.gender} selected disabled hidden>
-            {updateCatDetail.gender}
-          </option>
-          <option value="Male">Male</option>
-          <option value="Female">Female</option>
-          <option value="Unknown">Unknown</option>
-        </Select>
-        <Select name="adoptable" onChange={(event) => handleChange(event)}>
-          <option value={updateCatDetail.adoptable} selected disabled hidden>
-            {updateCatDetail.adoptable}
-          </option>
-          <option value="Yes">Yes</option>
-          <option value="No">No</option>
-        </Select>
-        <Select name="cage" onChange={(event) => handleChange(event)}>
-          <option value={updateCatDetail.cage} selected disabled hidden>
-            {updateCatDetail.cage}
-          </option>
-          <option value="6/7">6/7</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-          <option value="8">8</option>
-          <option value="9">9</option>
-        </Select>
-        </InputContainer>
+          <LabelContainer>
+            <Label>Name:</Label>
+            <DescriptionLabel>Description:</DescriptionLabel>
+            <Label>Image URL:</Label>
+            <Label>Gender:</Label>
+            <Label>Adoptable:</Label>
+            <Label>Cage:</Label>
+          </LabelContainer>
+          <InputContainer>
+            <Input
+              type="text"
+              name="name"
+              minLength="2"
+              value={updateCatDetail.name}
+              onChange={(event) => handleChange(event)}
+              required
+            />
+            <DescriptionInput
+              type="textarea"
+              name="description"
+              minLength="1"
+              value={updateCatDetail.description}
+              onChange={(event) => handleChange(event)}
+              required
+            />
+            <Input
+              type="text"
+              name="image"
+              minLength="5"
+              value={updateCatDetail.image}
+              onChange={(event) => handleChange(event)}
+            />
+            <Select name="gender" onChange={(event) => handleChange(event)}>
+              <option value={updateCatDetail.gender} selected disabled hidden>
+                {updateCatDetail.gender}
+              </option>
+              <option value="Male">Male</option>
+              <option value="Female">Female</option>
+              <option value="Unknown">Unknown</option>
+            </Select>
+            <Select name="adoptable" onChange={(event) => handleChange(event)}>
+              <option
+                value={updateCatDetail.adoptable}
+                selected
+                disabled
+                hidden
+              >
+                {updateCatDetail.adoptable}
+              </option>
+              <option value="Yes">Yes</option>
+              <option value="No">No</option>
+            </Select>
+            <Select name="cage" onChange={(event) => handleChange(event)}>
+              <option value={updateCatDetail.cage} selected disabled hidden>
+                {updateCatDetail.cage}
+              </option>
+              <option value="6/7">6/7</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+            </Select>
+          </InputContainer>
         </CatInfo>
         <Button type="submit" value="Update Cat">
           Update Cat
         </Button>
-        <Button href={"/cats/list"}>Cancel</Button>
       </Form>
+      <CancelLink to={"/cats/list"}>
+        <Button>Cancel</Button>
+      </CancelLink>
     </>
   );
 }
