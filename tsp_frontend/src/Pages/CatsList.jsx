617519df6df6d64c7e7c5231c64d1c2402929f3c
“soniatan” <sonnyeah@gmail.com>
2021-11-04 14:50:48 +0800

    added new card styling for each cat from map function, and commented out old styling

--- tsp_frontend/src/Pages/CatsList.jsx
+++ tsp_frontend/src/Pages/CatsList.jsx
@@ -2,17 +2,40 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link, useHistory } from "react-router-dom";
 import styled from "styled-components";
+import {
+  CardWrapper,
+  CardImage,
+  CardTextWrapper,
+  CardTextTitle,
+  CardStatWrapper,
+  CardStats,
+  LinkText,
+} from "../Components/CatCardStyle";
+import Tilt from "react-parallax-tilt";
 
 const LinkStyled = styled(Link)`
   text-decoration: none;
   color: black;
 `;
-const Img = styled.img`
-  border-radius: 50%;
-`;
+
+// const Img = styled.img`
+//   border-radius: 50%;
+// `;
 const Div = styled.div`
   display: inline-flex;
   margin: 5px;
+`;
+const CatWrapper = styled.div`
+width: 80%;
+margin: 5px auto;
+display: flex;
+flex-wrap: wrap;
+justify-content: space-around;
+`;
+const ContentContainer = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
 `;
 
 function CatsList({ role }) {
@@ -32,7 +55,7 @@ function CatsList({ role }) {
   const deleteCat = (id) => {
     axios.delete(`/api/cats/${id}`);
     window.alert(`Goodbye cat :(`);
-    setCats(cats.filter(cat=>cat._id !== id))
+    setCats(cats.filter((cat) => cat._id !== id));
   };
 
   const updateCat = (id) => {
@@ -42,7 +65,7 @@ function CatsList({ role }) {
   return (
     <>
       <h1>Cats</h1>
-      <div>
+      <ContentContainer>
         {/* Only allow admin to make new cat */}
         {role === "Admin" && (
           <>
@@ -51,11 +74,38 @@ function CatsList({ role }) {
             </LinkStyled>
           </>
         )}
+        <CatWrapper >
         {cats.map((element) => {
           return (
             <>
               <Div class="cats">
-                <p key={element._id}>
+                <Tilt key={element._id}>
+                  <CardWrapper>
+                      <CardImage background={element.image}>
+                      <Link to={`/cats/${element._id}`} />
+                        </CardImage>
+                    <CardTextWrapper>
+                      <CardTextTitle>{element.name}</CardTextTitle>
+                    </CardTextWrapper>
+                    {role === "Admin" && (
+                      <>
+                        <CardStatWrapper>
+                          <CardStats>
+                            <LinkText onClick={() => updateCat(element._id)}>
+                              Update
+                            </LinkText>
+                          </CardStats>
+                          <CardStats>
+                            <LinkText onClick={() => deleteCat(element._id)}>
+                              X
+                            </LinkText>
+                          </CardStats>
+                        </CardStatWrapper>
+                      </>
+                    )}
+                  </CardWrapper>
+                </Tilt>
+                {/* <p key={element._id}>
                   <LinkStyled to={`/cats/${element._id}`}>
                     <Img
                       src={element.image}
@@ -68,7 +118,7 @@ function CatsList({ role }) {
                   {element.name}
                   <br />
                   {/* Only allow delete/update if admin */}
-                  {role === "Admin" && (
+                {/* {role === "Admin" && (
                     <>
                       <button onClick={() => updateCat(element._id)}>
                         Update
@@ -76,12 +126,13 @@ function CatsList({ role }) {
                       <button onClick={() => deleteCat(element._id)}>X</button>
                     </>
                   )}
-                </p>
+                </p> */}
               </Div>
             </>
           );
         })}
-      </div>
+        </CatWrapper>
+      </ContentContainer>
     </>
   );
 }
